49d6da936096b4a2f379fc29fabe60df75623a55
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2019-11-22 21:09:37 -0300

    deploy

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,9 @@
+import * as _ from "lodash";
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import * as _ from "lodash";
-import { ToastrService } from 'ngx-toastr';
 
 export interface check_email {
   success: Boolean,
--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-user',
@@ -9,11 +12,16 @@ import { ToastrService } from 'ngx-toastr';
 })
 
 export class UserComponent implements OnInit {
+
+  exists: Boolean;
+  form: FormGroup;
   users: any;
 
   constructor(
     private user: UserService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private builder: FormBuilder,
+    private auth: AuthService
   ) { }
 
   ngOnInit() {
@@ -23,7 +31,32 @@ export class UserComponent implements OnInit {
     }, e => {
       console.log(e.error);
     });
+
+    this.form = this.builder.group({
+      name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      username: ['', Validators.required],
+      phone: ['', Validators.required],
+      password: ['', [Validators.minLength(6)]],
+      email: ['', [Validators.email]],
+      role: ['', Validators.required]
+    });
+
   }
+
+  public create(form) {
+
+  }
+
+  public email(email) {
+    this.auth.check(email).subscribe(response => {
+      this.exists = true;
+    }, e => {
+      this.exists = false;
+    });
+  }
+
+  check = _.debounce(() => this.email(this.form.value), 1000);
 
   public remove(id) {
     return this.user.remove(id).subscribe(response => {
